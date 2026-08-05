c966576a7679134fea09542ac5e3e7e854f22cda
everidiy <itusitas@gmail.com>
2026-08-05 18:34:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -138,6 +138,18 @@ function initSendButton() {
     });
 }
 
+async function loadImage(imgEl, fileId) {
+    try {
+        const res = await fetch(`https://tweezers-glorious-slimness.ngrok-free.dev/api/image/${encodeURIComponent(fileId)}`, {
+            headers: { "ngrok-skip-browser-warning": "true" }
+        });
+        const blob = await res.blob();
+        imgEl.src = URL.createObjectURL(blob);
+    } catch (e) {
+        console.error("image load failed", e);
+    }
+}
+
 async function loadCardsFromServer() {
     const userId  = 5906060490;
 
@@ -184,7 +196,7 @@ async function loadCardsFromServer() {
 
             // ИСПРАВЛЕНО: Правильный адрес к эндпоинту картинок C# через ngrok с знаком $
             const imageHtml = photoFileId
-                ? `<img src="https://tweezers-glorious-slimness.ngrok-free.dev/api/image/${encodeURIComponent(photoFileId)}?ngrok-skip-browser-warning=true" alt="${cardName}" class="can-img" />`
+                ? `<img alt="${cardName}" class="can-img" />`
                 : `<div class="card-image-placeholder">🥤</div>`;
 
             // ИСПРАВЛЕНО: Вставлена переменная imageHtml, закрыт тег <h3> и убрано "/10" у типа
@@ -206,6 +218,12 @@ async function loadCardsFromServer() {
                 </div>
             `;
             grid.appendChild(cardElement);
+
+            if (photoFileId) {
+                const imgEl = cardElement.querySelector("img");
+                if (imgEl) loadImage(imgEl, photoFileId);
+            }
+            
             console.log(cardElement.innerHTML);
             console.log(cardElement.querySelector("img")?.src);
         });
